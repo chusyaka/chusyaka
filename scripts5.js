ee1b602010c49ce2324f2b7f8a5b9a8f9e09d0db
chusyaka <kirillchusov44@gmail.com>
2025-09-18 17:20:09 +0300

    Update scripts5.js

--- scripts5.js
+++ scripts5.js
@@ -1,37 +1,58 @@
-let num1 = null;
-let num2 = null;
+let num1 = '';
+let num2 = '';
+let operator = null;
 let result = null;
 
 let display = document.getElementById('display');
-       
-        function but(value) {
-            display.value += value;
-        }
-        
-        function clearDisplay() {
-           display.value = '';
-        }
-        function setOperation(value) {
-            display.value += value;
-        }
 
-        function calculate() {
-            
-            switch(setOperation) {
-                case"+":
-                result = num1 + num2;
-                console.log("сумма" + result)
-                break;
-                case"-":
-                result = num1 - num2;
-                break;
-                case"*":
-                result = num1 * num2;
-                break;
-                case"/":
-                result = num1 / num2;
-                break;
-            }
-            display.value = result
-        }
-        
+function but(value) {
+    display.value += value;
+    
+    if (operator === null) {
+        num1 += value;
+    } else {
+        num2 += value;
+    }
+}
+
+function clearDisplay() {
+    display.value = '';
+    num1 = '';
+    num2 = '';
+    operator = null;
+}
+
+function setOperation(value) {
+    if (operator !== null && num2 !== '') {
+        calculate();
+    }
+    
+    display.value += value;
+    operator = value;
+}
+
+function calculate() {
+    if (num1 === '' ||  num2 === '' ||  operator === null) {
+        return; 
+    }
+    
+    const number1 = parseFloat(num1);
+    const number2 = parseFloat(num2);
+    
+    switch(operator) {
+        case '+':
+            result = number1 + number2;
+            break;
+        case '-':
+            result = number1 - number2;
+            break;
+        case '*':
+            result = number1 * number2;
+            break;
+        case '/':
+                result = number1 / number2;
+            break;
+    }
+    
+    display.value = result;
+}
